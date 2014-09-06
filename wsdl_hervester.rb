9d8fde292372b454599a38f11f1393339917f4d2
Mirza Arifovic <rimzoni@gmail.com>
2014-09-06 14:12:21 +0200

    Added wsdl_hervester.rb

---
+++ wsdl_hervester.rb
@@ -0,0 +1,25 @@
+# Class of an Hervester or WSDL files
+
+Class WSDLHervester
+
+# method used for initialization of an class
+def initialize
+  
+end
+
+# method used for download of an wsdl file
+def downloader(file_location)
+  
+end
+
+# method used to parse wsdl file
+def parser
+  
+end
+
+# method used to display
+def method_name
+  
+end
+
+end
